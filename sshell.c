184da9a6f34acbd82962b11e81a176a8fc4419f8
mingc <mingc@ad3.ucdavis.edu@pc24.cs.ucdavis.edu>
2019-04-09 22:54:45 -0700

    running commands the hard way

--- sshell.c
+++ sshell.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
-	char *cmd = "/bin/date -u";
-	int retval;
+	char *cmd[3] = { "/bin/date", "-u", NULL };
+	pid_t pid;
+	int status;
 
-	retval = system(cmd);
-	fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
-
+	pid = fork();
+	if(pid == 0) {
+		/* Child */
+		execvp(cmd[0], cmd);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	} else if(pid > 0) {
+		/* Parent */
+		waitpid(-1, &status, 0);
+		printf("Return status value for '%s': %d\n", cmd[0], status);
+	} else {
+                /* fork error */
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 	return EXIT_SUCCESS;
 }
