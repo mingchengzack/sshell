3902b168e739a79de8551742840b87b7ad6894df
mingc <mingc@ad3.ucdavis.edu@pc24.cs.ucdavis.edu>
2019-04-14 22:28:11 -0700

    Completed phase 6

--- sshell.c
+++ sshell.c
@@ -48,10 +48,10 @@ enum {
     NOT_BUILTIN
 };
 
-/* commandline strcut */
-struct commandline {
-    char command[MAX_CMD];		/* the whole command line */
-    char *args[MAX_ARGS];		/* arguments of the command line */
+/* command strcut */
+struct command {
+    char command[MAX_CMD];		/* the whole command */
+    char *args[MAX_ARGS];		/* arguments of the command */
     int num_input;			/* number of input redirection */
     int num_output;			/* number of output redirection */
     char input_file[MAX_ARGS][MAX_CMD];	/* the array of the input files */
@@ -59,15 +59,23 @@ struct commandline {
     int num_args;			/* number of arguments in the command line */
 };
 
+/* job struct */
+struct job {
+    char commandline[MAX_CMD];		/* the total command line */
+    struct command processes[MAX_CMD];	/* a job contains one or more commands */
+    int num_commands;			/* number of commands/processes */
+};
+
 /*************************************************************
  *                    LOCAL FUNCTION PROTOTYPES              *
  *************************************************************/
 
 char find_closest_occurence(char *array, const char *delim);
-void read_command(struct commandline *cmd);
-void free_command(struct commandline *cmd);
-int is_valid_command(const struct commandline cmd);
-int is_builtin_command(const struct commandline cmd);
+void read_job(struct job *job);
+void read_command(struct command *cmd);
+void free_command(struct command *cmd);
+int is_valid_command(const struct command cmd);
+int is_builtin_command(const struct command cmd);
 int cd(const char *dir);
 int pwd();
 void redirection(int mode, const char files[MAX_ARGS][MAX_CMD], int num_files);
@@ -79,34 +87,41 @@ void error_message(int error_code);
  *************************************************************/
 
 /*
- * This function finds a character from delimiters that is the closest occurence in the array
- * @param - {char *} - the array to find closest occurence of char in delimiters
- *        - {const char *} - the array of delimiters to find occurence
- *
- * @return - the close occurence character in delim
- */
-char find_closest_occurence(char *array, const char *delim) {
-    int min = MAX_CMD;
-    char closest_occurence = MAX_CHAR;
-    int i;
-    char *token;
-    
-    for(i = 0; i < strlen(delim); i++) {
-	token = strchr(array, delim[i]);
-	if(token != NULL && (token - array) < min) {
-            min = token - array;	   /* update min value */
-	    closest_occurence = delim[i];  /* update closet occurence of char */
-	}	
-    }
-    return closest_occurence;
-}
-
-/*
- * This function reads the command from terminal
- * @param - {struct *} - the command line struct
+ * This function reads the whole command line from terminal and store each command
+ * @param - {job *} - the job to store one or more commands (pipeline) 
  * @return - none
  */
-void read_command(struct commandline *cmd) {
+void read_job(struct job *job) {
+    char commands[MAX_CMD];
+   // char command[MAX_CMD];
+    char *token;
+
+    fgets(commands, MAX_CMD, stdin);		/* get the entire command line */
+    commands[strlen(commands) - 1] = 0;		/* get rid of newline */
+    job->num_commands = 0;			/* initialize number of command */
+
+    token = strtok(commands, "|");		/* get the first command */
+    if(token == NULL) {				/* only one or zero command */
+    	if(commands[0] != 0) {			/* one command */
+	    read_command(&(job->processes[0]));
+	} else {				/* no command */
+            //job->processes[0] = NULL;	
+	}
+    }
+    while(token != NULL) {
+	read_command(&(job->processes[job->num_commands++]));
+	token = strtok(NULL, "|");	
+    }
+    return;
+}	
+
+/*
+ * This function parses the command and store it
+ * @param - {struct *} - the command struct
+ *        - {char *} - the command to be parsed
+ * @return - none
+ */
+void read_command(struct command *cmd) {
     char command[MAX_CMD], arg[MAX_CMD];
     int num_white_space;
 
@@ -123,46 +138,64 @@ void read_command(struct commandline *cmd) {
 
     /* get rid of trailing spaces */
     int i;
-    for(i = strlen(command) - 1; command[i] == ' '; i--)
+    for(i = strlen(command) - 1; i >= 0 && command[i] == ' '; i--)
         command[i] = 0;
 
-    /* parse the command line */
+    /* parse the command */
     int read_code = ARGUMENT;
     i = 0;
     while(i < strlen(command)) {
         memset(arg, 0, MAX_CMD);
 	int j = 0;
-	/* get the argument  */
+
+	/* get the argument */
         while(command[i] != ' ' && command[i] != '<' && command[i] != '>' && command[i] != 0) {
             arg[j++] = command[i++];
 	}
+	arg[j] = 0;				/* add null terminator */
+
         for(; command[i] == ' '; i++);		/* get rid of leading spaces */
       
 	switch(read_code) {
             case ARGUMENT:	/* an argument for the program */
 	        cmd->args[cmd->num_args] = (char *) malloc((strlen(arg) + 1));
-		strcpy(cmd->args[cmd->num_args++], arg);
+         	strcpy(cmd->args[cmd->num_args++], arg);
 		break;
 	    case INPUT:		/* input file */
-		strcpy(cmd->input_file[cmd->num_input++], arg);
+		if(arg[0] == 0) {
+		    strcpy(cmd->input_file[cmd->num_input++], " ");
+	        } else {
+		    strcpy(cmd->input_file[cmd->num_input++], arg);
+                }
 		read_code = ARGUMENT;
 	        break;
 	    case OUTPUT:	/* output file */
-        	strcpy(cmd->output_file[cmd->num_output++], arg);
+		if(arg[0] == 0) {
+                    strcpy(cmd->output_file[cmd->num_output++], " ");
+                } else {
+                    strcpy(cmd->output_file[cmd->num_output++], arg);
+                }
 		read_code = ARGUMENT;
 	        break;
 	}
 
 	/* check for redirections */
-	if(command[i] == '<') { /* read input file for next argument */
+	if(command[i] == '<') {			/* read input file for next argument */
             read_code = INPUT;
             i++;
-            for(; command[i] == ' '; i++);          /* get rid of leading spaces */
-        } else if(command[i] == '>') {  /* read output file for next argument */
+            for(; command[i] == ' '; i++);	/* get rid of leading spaces */
+        } else if(command[i] == '>') {		/* read output file for next argument */
             read_code = OUTPUT;
             i++;
-            for(; command[i] == ' '; i++);          /* get rid of leading spaces */
+            for(; command[i] == ' '; i++);	/* get rid of leading spaces */
         }
+    }
+    
+    /* check given files */
+    if(read_code == INPUT) {
+        strcpy(cmd->input_file[cmd->num_input++], " ");	 /* use emtpy string to indicate not given file */
+    } else if (read_code == OUTPUT) {
+    	strcpy(cmd->output_file[cmd->num_output++], " "); /* use emtpy string to indicate not given file */	
     }
     
     cmd->args[cmd->num_args] = NULL;		/* set null terminator */
@@ -174,7 +207,7 @@ void read_command(struct commandline *cmd) {
  * @param - {struct *} - the command line struct
  * @return - none
  */
-void free_command(struct commandline *cmd) {
+void free_command(struct command *cmd) {
     int i;
     for(i = 0; i < (cmd->num_args); i++) {
         free(cmd->args[i]);			/* free the allocated memory for each argument */
@@ -187,7 +220,7 @@ void free_command(struct commandline *cmd) {
  * @param - {const struct} - the command line struct
  * @return - {int} - error code
  */
-int is_valid_command(const struct commandline cmd) {
+int is_valid_command(const struct command cmd) {
     return 0;
 }
 
@@ -196,7 +229,7 @@ int is_valid_command(const struct commandline cmd) {
  * @param - {const struct} - the command line struct
  * @return - {int} - builtin command enum
  */
-int is_builtin_command(const struct commandline cmd) {
+int is_builtin_command(const struct command cmd) {
     if(strcmp(cmd.args[0], "exit") == 0) {		/* exit */
         return EXIT;
     } else if(strcmp(cmd.args[0], "cd") == 0) {		/* cd */
@@ -272,7 +305,7 @@ int check_redirection_file(int mode, const char files[MAX_ARGS][MAX_CMD], int nu
     for(i = 0; i < num_files; i++) {
         /* check if the file name is given */
         if(strcmp(files[i], " ") == 0) {
-            return mode == INPUT ? ERR_NO_INPUTFILE : ERR_NO_OUTPUTFILE;
+            return (mode == INPUT) ? ERR_NO_INPUTFILE : ERR_NO_OUTPUTFILE;
         }
     
         /* check if the file can be opened */
@@ -342,10 +375,12 @@ int main(int argc, char *argv[])
     int status;
 
     while(1) {
-        struct commandline cmd;
+        struct command cmd;
+//	struct job job;
 	int builtin_command_code;
 
 	printf("sshell$ ");		/* Display prompt */
+//	read_job(&job);			/* read the job */
 	read_command(&cmd);		/* read command from stdin */
 
 	/* no command is entered */
@@ -355,18 +390,6 @@ int main(int argc, char *argv[])
 	
 	/* check if it is a built in command */
 	builtin_command_code = is_builtin_command(cmd);
-	if(builtin_command_code == EXIT) {		/* leave the shell */
-	    fprintf(stderr, "Bye...\n");
-	    exit(EXIT_SUCCESS);
-	} else if(builtin_command_code == CD) {         /* run cd command */
-            status = cd(cmd.args[1]);
-            fprintf(stderr, "+ completed '%s' [%d]\n", cmd.command, status);
-	    continue;
-	} else if(builtin_command_code == PWD) {    	/* run pwd command */
-            status = pwd();
-	    fprintf(stderr, "+ completed '%s' [%d]\n", cmd.command, status);
-            continue;
-	}
 
 	/* check if it has input redirection */
         if(cmd.num_input > 0) {
@@ -379,7 +402,7 @@ int main(int argc, char *argv[])
 	}
 
 	/* check if it has output redirection */
-        if(cmd.num_output < 0) {
+        if(cmd.num_output > 0) {
             /* check if it has errors */
             int error_code = check_redirection_file(OUTPUT, cmd.output_file, cmd.num_output);
             if(error_code != SUCCESS) {
@@ -388,10 +411,22 @@ int main(int argc, char *argv[])
             }
         }
 
+	/* check if it is builtin command */
+        if(builtin_command_code == EXIT) {          /* leave the shell */
+            fprintf(stderr, "Bye...\n");
+            exit(EXIT_SUCCESS);
+        }
+        else if(builtin_command_code == CD) {    /* run cd command */
+            status = cd(cmd.args[1]);
+
+        } else if(builtin_command_code == PWD) {    /* run pwd command */
+            status = pwd();
+        }
+
 	/* run not-built-in command */
 	pid = fork();			/* fork child process */
 	if(pid == 0) {			/* Child */
-            /* perform input redirection */
+	    /* perform input redirection */
 	    if(cmd.num_input > 0) {
                 redirection(INPUT, cmd.input_file, cmd.num_input);
             } 
@@ -400,11 +435,15 @@ int main(int argc, char *argv[])
 	    if(cmd.num_output > 0) {
                 redirection(OUTPUT, cmd.output_file, cmd.num_output);
 	    }
-
-	    execvp(cmd.args[0], cmd.args);		
-	    /* execvp error */
-	    error_message(ERR_CMD_NOTFOUND);
-	    exit(EXIT_FAILURE);
+	    
+	    if(builtin_command_code == NOT_BUILTIN) {	/* not builtin command */
+	        execvp(cmd.args[0], cmd.args);		
+	        /* execvp error */
+	        error_message(ERR_CMD_NOTFOUND);
+	        exit(EXIT_FAILURE);
+	    } else {					/* builtin command */
+                exit(status);
+            }
 	} else if(pid > 0) {		/* Parent */
 	    waitpid(-1, &status, 0);		/* wait for child to complete */
 	    /* Information message after execution */
